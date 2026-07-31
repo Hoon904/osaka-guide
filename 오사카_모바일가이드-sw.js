e1e50adf10f27fe5d7ea4b2366eeb6a7d308227a
Hoon904 <lhy904@gmail.com>
2026-08-01 00:18:19 +0900

    Finalize Osaka mobile guide

--- 오사카_모바일가이드-sw.js
+++ 오사카_모바일가이드-sw.js
@@ -1,33 +1,25 @@
-const CACHE = "osaka-guide-v1";
+const CACHE = "osaka-guide-v4";
 const ASSETS = [
+  "./",
   "./오사카_모바일가이드.html",
   "./오사카_모바일가이드.webmanifest",
-  "./오사카_모바일가이드-icon.svg"
+  "./오사카_모바일가이드-icon.svg",
+  "./travel-data.js",
+  "./travel-guide-ui.js"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
-    )
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))));
   self.clients.claim();
 });
-
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-  event.respondWith(
-    caches.match(event.request).then(cached =>
-      cached || fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        return response;
-      }).catch(() => caches.match("./오사카_모바일가이드.html"))
-    )
-  );
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    caches.open(CACHE).then(cache => cache.put(event.request, response.clone()));
+    return response;
+  }).catch(() => caches.match("./오사카_모바일가이드.html"))));
 });
